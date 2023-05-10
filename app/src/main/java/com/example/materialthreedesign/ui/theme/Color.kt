12dafb4f52087daaeb951a79caf02434c7beac81
AkashMadanu <akashmadanu2004@gmail.com>
2023-05-10 17:09:58 +0530

    Added Colors

---
+++ app/src/main/java/com/example/materialthreedesign/ui/theme/Color.kt
@@ -0,0 +1,40 @@
+package com.example.materialthreedesign.ui.theme
+
+import androidx.compose.ui.graphics.*
+
+
+val Green10 = Color(0xFF003300)
+val Green20 = Color(0xFF006600)
+val Green30 = Color(0xFF009900)
+val Green40 = Color(0xFF00cc00)
+val Green80 = Color(0xFF99ff99)
+val Green90 = Color(0xFFccffcc)
+
+
+val DarkGreen10 = Color(0xFF003300)
+val DarkGreen20 = Color(0xFF006600)
+val DarkGreen30 = Color(0xFF009900)
+val DarkGreen40 = Color(0xFF00cc00)
+val DarkGreen80 = Color(0xFF99ff99)
+val DarkGreen90 = Color(0xFFccffcc)
+
+val Voilet10 = Color(0xFF330033)
+val Voilet20 = Color(0xFF660066)
+val Voilet30 = Color(0xFF990099)
+val Voilet40 = Color(0xFFcc00cc)
+val Voilet80 = Color(0xFFff99ff)
+val Voilet90 = Color(0xFFffccff)
+
+val Grey10 = Color(0xFF1a1a1a)
+val Grey20 = Color(0xFF333333)
+val Grey90 = Color(0xFFe6e6e6)
+val Grey95 = Color(0xFFf2f2f2)
+val Grey99 = Color(0xFFFAF9F9)
+
+
+
+val GreenGrey30 = Color(0xFF00997a)
+val GreenGrey50 = Color(0xFF00ffcc)
+val GreenGrey60 = Color(0xFF33ffd6)
+val GreenGrey80 = Color(0xFF99ffeb)
+val GreenGrey90 = Color(0xFFccfff5)
