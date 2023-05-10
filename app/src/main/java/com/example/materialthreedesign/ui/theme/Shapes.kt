ee626abdb1f7e9c8dd44ca2ff1bde8ab458abc41
AkashMadanu <akashmadanu2004@gmail.com>
2023-05-10 22:02:08 +0530

    Colors Updated,Makeing Card

--- app/src/main/java/com/example/materialthreedesign/ui/theme/Shapes.kt
+++ app/src/main/java/com/example/materialthreedesign/ui/theme/Shapes.kt
@@ -1,2 +1,11 @@
 package com.example.materialthreedesign.ui.theme
 
+import androidx.compose.foundation.shape.*
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.*
+
+val Shapes = Shapes(
+        small = RoundedCornerShape(4.dp),
+        medium = RoundedCornerShape(4.dp),
+        large = RoundedCornerShape(0.dp)
+)
